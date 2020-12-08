4e5eea1d9fb7967a4d1356fe0459970ac0c1f79f
sebas <mab.tromper@student.avans.nl>
2020-12-08 11:02:00 +0100

    [Added] Created all fragments

---
+++ app/src/main/java/com/a1/nextlocation/CouponFragment.java
@@ -0,0 +1,24 @@
+package com.a1.nextlocation;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CouponFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_coupon, container, false);
+    }
+}
