16b9b15856ca49054b99986cb43afed642fbfb67
RemoMeijer <remcomeijer0210@gmail.com>
2020-12-14 12:05:32 +0100

    Added some functionality

--- app/src/main/java/com/a1/nextlocation/recyclerview/LocationAdapter.java
+++ app/src/main/java/com/a1/nextlocation/recyclerview/LocationAdapter.java
@@ -1,12 +1,15 @@
 package com.a1.nextlocation.recyclerview;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.a1.nextlocation.R;
 import com.a1.nextlocation.data.Location;
 
 import java.util.List;
@@ -23,31 +26,41 @@ public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.Locati
 
     class LocationViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+        private TextView locationName;
+
         public LocationViewHolder(@NonNull View itemView) {
             super(itemView);
+            this.locationName = itemView.findViewById(R.id.location_name);
         }
 
         @Override
         public void onClick(View view) {
             clickListener.onItemClick(getAdapterPosition());
         }
+
+        public void setTextViewText(String text){
+            locationName.setText(text);
+        }
     }
 
     public LocationAdapter(Context context, List<Location> location, CouponAdapter.OnItemClickListener listener){
-        appContext = context;
-        locationList = location;
-        clickListener = listener;
+        this.appContext = context;
+        this.locationList = location;
+        this.clickListener = listener;
     }
 
     @NonNull
     @Override
     public LocationAdapter.LocationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_location, parent, false);
+        LocationViewHolder viewHolder = new LocationViewHolder(itemView);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull LocationAdapter.LocationViewHolder holder, int position) {
-
+        Location location = locationList.get(position);
+        holder.setTextViewText(location.getName());
     }
 
     @Override
