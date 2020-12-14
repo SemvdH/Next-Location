a2ffef10999cbd40cab4b882e418bb9fa356ce54
Bart <bartvanmoorsel7@gmail.com>
2020-12-14 11:04:31 +0100

    [ADD] data & location class fields and getters / setters

--- app/src/main/java/com/a1/nextlocation/data/Data.java
+++ app/src/main/java/com/a1/nextlocation/data/Data.java
@@ -1,4 +1,71 @@
 package com.a1.nextlocation.data;
 
+import java.util.List;
+
 public class Data {
+    private float distanceTraveled;
+    private int locationsVisited;
+    private int totalTime;
+    private List<Coupon> couponList;
+    private Location nextLocation;
+    private Location lastLocation;
+    private Route currentRoute;
+
+    
+
+    public float getDistanceTraveled() {
+        return distanceTraveled;
+    }
+
+    public void setDistanceTraveled(float distanceTraveled) {
+        this.distanceTraveled = distanceTraveled;
+    }
+
+    public int getLocationsVisited() {
+        return locationsVisited;
+    }
+
+    public void setLocationsVisited(int locationsVisited) {
+        this.locationsVisited = locationsVisited;
+    }
+
+    public int getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(int totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public List<Coupon> getCouponList() {
+        return couponList;
+    }
+
+    public void setCouponList(List<Coupon> couponList) {
+        this.couponList = couponList;
+    }
+
+    public Location getNextLocation() {
+        return nextLocation;
+    }
+
+    public void setNextLocation(Location nextLocation) {
+        this.nextLocation = nextLocation;
+    }
+
+    public Location getLastLocation() {
+        return lastLocation;
+    }
+
+    public void setLastLocation(Location lastLocation) {
+        this.lastLocation = lastLocation;
+    }
+
+    public Route getCurrentRoute() {
+        return currentRoute;
+    }
+
+    public void setCurrentRoute(Route currentRoute) {
+        this.currentRoute = currentRoute;
+    }
 }
--- app/src/main/java/com/a1/nextlocation/data/Location.java
+++ app/src/main/java/com/a1/nextlocation/data/Location.java
@@ -1,4 +1,31 @@
 package com.a1.nextlocation.data;
 
 public class Location {
+    private String name;
+    private String coordinates;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(String coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
