fac41e2adec6c580d1f81f69774f1865e867b80e
Sem van der Hoeven <semmer99@gmail.com>
2020-12-14 10:55:39 +0100

    [ADD] route class fields and getters / setters

--- app/src/main/java/com/a1/nextlocation/data/Route.java
+++ app/src/main/java/com/a1/nextlocation/data/Route.java
@@ -1,4 +1,42 @@
 package com.a1.nextlocation.data;
 
+import java.util.List;
+
 public class Route {
+    private String name;
+    private List<Location> locations;
+    private float totalDistance;
+    private int totalTime;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Location> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(List<Location> locations) {
+        this.locations = locations;
+    }
+
+    public float getTotalDistance() {
+        return totalDistance;
+    }
+
+    public void setTotalDistance(float totalDistance) {
+        this.totalDistance = totalDistance;
+    }
+
+    public int getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(int totalTime) {
+        this.totalTime = totalTime;
+    }
 }
