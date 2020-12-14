dec62e8b9ad0c1a18462b42ce0e66e9aebee19d2
Sem van der Hoeven <semmer99@gmail.com>
2020-12-14 10:56:49 +0100

    [ADD] coupon class fields and getters / setters

--- app/src/main/java/com/a1/nextlocation/data/Coupon.java
+++ app/src/main/java/com/a1/nextlocation/data/Coupon.java
@@ -1,4 +1,23 @@
 package com.a1.nextlocation.data;
 
 public class Coupon {
+
+    private String code;
+    private String reward;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getReward() {
+        return reward;
+    }
+
+    public void setReward(String reward) {
+        this.reward = reward;
+    }
 }
