9e8e9f4cdd6fa8d33054c3db7f9af4ac68e0202d
Sem van der Hoeven <semmer99@gmail.com>
2020-12-17 11:00:14 +0100

    changed loader stuff

--- app/src/main/java/com/a1/nextlocation/recyclerview/LocationLoader.java
+++ app/src/main/java/com/a1/nextlocation/recyclerview/LocationLoader.java
@@ -2,10 +2,12 @@ package com.a1.nextlocation.recyclerview;
 
 import com.a1.nextlocation.data.Location;
 
-public class LocationLoader implements Loader<Location>{
+import java.util.List;
+
+public class LocationLoader implements Loader<List<Location>>{
 
     @Override
-    public Location load() {
+    public List<Location> load() {
         return null;
     }
 }
