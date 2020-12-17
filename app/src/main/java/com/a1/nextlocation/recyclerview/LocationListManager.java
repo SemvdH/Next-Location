1f835a94b099945450e25edb16b949225be41045
RemoMeijer <remcomeijer0210@gmail.com>
2020-12-17 11:16:03 +0100

    Updated managers so they can load

--- app/src/main/java/com/a1/nextlocation/recyclerview/LocationListManager.java
+++ app/src/main/java/com/a1/nextlocation/recyclerview/LocationListManager.java
@@ -1,22 +1,31 @@
 package com.a1.nextlocation.recyclerview;
 
+import android.content.Context;
+
 import com.a1.nextlocation.data.Location;
 
 import java.util.List;
 
 public class LocationListManager {
 
-    private List<Location> location;
+    private List<Location> locationList;
+    private Context context;
 
-    public LocationListManager(){
+    public LocationListManager(Context context){
+        this.context = context;
 
     }
 
-    public List<Location> getLocation() {
-        return location;
+    public List<Location> getLocationList() {
+        return locationList;
     }
 
-    public void setLocation(List<Location> location) {
-        this.location = location;
+    public Location getLocation(int place) {
+        return locationList.get(place);
+    }
+
+    public void load() {
+        LocationLoader locationLoader = new LocationLoader(this.context);
+        this.locationList = locationLoader.load();
     }
 }
