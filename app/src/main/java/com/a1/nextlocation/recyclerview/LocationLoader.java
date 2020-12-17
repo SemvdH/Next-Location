12c8d6a62dc71a070fe6ffbe4298b76cc722303c
Sem van der Hoeven <semmer99@gmail.com>
2020-12-17 11:14:43 +0100

    [ADD] loaders and fileio read

--- app/src/main/java/com/a1/nextlocation/recyclerview/LocationLoader.java
+++ app/src/main/java/com/a1/nextlocation/recyclerview/LocationLoader.java
@@ -1,13 +1,27 @@
 package com.a1.nextlocation.recyclerview;
 
+import android.content.Context;
+
+import com.a1.nextlocation.data.FileIO;
 import com.a1.nextlocation.data.Location;
 
 import java.util.List;
 
 public class LocationLoader implements Loader<List<Location>>{
+    private final Context context;
+
+    public LocationLoader(Context context) {
+        this.context = context;
+    }
 
     @Override
     public List<Location> load() {
+        FileIO<List<Location>> fileIO = new FileIO<>();
+        try {
+            return fileIO.readFileData(context,"locations.json").newInstance();
+        } catch (IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
--- app/src/main/java/com/a1/nextlocation/recyclerview/RouteLoader.java
+++ app/src/main/java/com/a1/nextlocation/recyclerview/RouteLoader.java
@@ -1,8 +1,28 @@
 package com.a1.nextlocation.recyclerview;
 
-public class RouteLoader implements Loader {
-    @Override
-    public void load() {
+import android.content.Context;
 
+import com.a1.nextlocation.data.FileIO;
+import com.a1.nextlocation.data.Location;
+import com.a1.nextlocation.data.Route;
+
+import java.util.List;
+
+public class RouteLoader implements Loader<List<Route>> {
+    private final Context context;
+    public RouteLoader(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public List<Route> load() {
+
+        FileIO<List<Route>> fileIO = new FileIO<>();
+        try {
+            return fileIO.readFileData(context,"routes.json").newInstance();
+        } catch (IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
