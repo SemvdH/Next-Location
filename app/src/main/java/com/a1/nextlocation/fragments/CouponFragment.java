21183368155af4e6cc66da713e3fc28fdbf00b4a
RemoMeijer <remcomeijer0210@gmail.com>
2021-01-04 11:40:06 +0100

    Switching fragment from statistics to coupon works

--- app/src/main/java/com/a1/nextlocation/fragments/CouponFragment.java
+++ app/src/main/java/com/a1/nextlocation/fragments/CouponFragment.java
@@ -34,7 +34,7 @@ public class CouponFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_coupon, container, false);
 
-        this.couponRecyclerView = view.findViewById(R.id.routeRecyclerView);
+        this.couponRecyclerView = view.findViewById(R.id.couponRecyclerView);
         this.couponRecyclerView.setHasFixedSize(true);
         this.layoutManager = new LinearLayoutManager(this.getContext());
 
--- app/src/main/java/com/a1/nextlocation/fragments/StatisticFragment.java
+++ app/src/main/java/com/a1/nextlocation/fragments/StatisticFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,13 +19,13 @@ public class StatisticFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_statistic, container, false);
+
+
         ConstraintLayout constraintLayout = view.findViewById(R.id.Box4);
         constraintLayout.setOnClickListener(v -> {
             CouponFragment couponFragment = new CouponFragment();
