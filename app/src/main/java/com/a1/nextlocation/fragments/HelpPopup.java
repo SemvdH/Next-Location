7e99070002127a94ef8acdd774ed15f8758ea3aa
Sem van der Hoeven <semmer99@gmail.com>
2021-01-07 11:20:48 +0100

    added ok button to help popup

--- app/src/main/java/com/a1/nextlocation/fragments/HelpPopup.java
+++ app/src/main/java/com/a1/nextlocation/fragments/HelpPopup.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.DialogFragment;
 
@@ -15,6 +16,8 @@ public class HelpPopup extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.help_popup, container, false);
         getDialog().setTitle("Simple Dialog");
+        Button okButton = rootView.findViewById(R.id.help_ok_button);
+        okButton.setOnClickListener(v -> dismiss());
         return rootView;
     }
 }
